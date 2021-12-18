68c718f5fd57b7457b2760393240912d6a7da9ac
wonju-dev <wonjulee.dev@gmail.com>
2021-12-18 16:44:34 +0900

    [Refactor] Fetch에서 form tag으로 Post Request 방식 변경

--- index.ts
+++ index.ts
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/", mainPageRouter);
 app.use("/login", authRouter);
--- routes/auth.ts
+++ routes/auth.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.post("/", (req, res) => {
-  console.log(req.body);
+  res.send(req.body);
 });
 
 export default router;
